e1928fa6bb8e5fb973af0ddc80fc41460ebe01b5
Matteo <matteo.violani@studio.unibo.it>
2020-12-28 14:55:11 +0100

    mvcio2: add extension to mvcio controller to read all text on file

---
+++ src/it/unibo/oop/lab/mvcio2/ControllerExtended.java
@@ -0,0 +1,13 @@
+package it.unibo.oop.lab.mvcio2;
+
+import it.unibo.oop.lab.mvcio.Controller;
+
+public class ControllerExtended extends Controller {
+    /**
+     *
+     * @return text reader from file
+     * */
+    public String getAllText() {
+        return null;
+    }
+}
