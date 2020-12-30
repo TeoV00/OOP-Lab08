dff687a6b72230a996877690113f6236a654caf9
Teo <matteo.violani@studio.unibo.it>
2020-12-30 13:16:09 +0100

    advance: add tag parser in SettingLoader, new private method

--- src/it/unibo/oop/lab/advanced/SettingLoader.java
+++ src/it/unibo/oop/lab/advanced/SettingLoader.java
@@ -1,24 +1,49 @@
 package it.unibo.oop.lab.advanced;
 
-import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class SettingLoader {
 
+    private static final String COLON = ":";
+    private static final String SPACE = " ";
+    private static final String MIN_TAG = "minimum";
+    private static final String MAX_TAG = "maximum";
+    private static final String ATTEMP_TAG = "attempts";
     private int min;
     private int max;
     private int attemps;
 
     public SettingLoader(){
-        try(InputStream in = new BufferedInputStream(ClassLoader.getSystemResourceAsStream("res/config.yml"))){
-            while 
+        final InputStream inStream = ClassLoader.getSystemResourceAsStream("config.yml");
+        
+        try ( BufferedReader in = new BufferedReader(new InputStreamReader(inStream))) {
+            String line = in.readLine();
+            while (line != null) {
+                if(line.startsWith(MIN_TAG)) {
+                    this.min = this.getValueFromLine(line, MIN_TAG);
+                }
+                if(line.startsWith(MAX_TAG)) {
+                    this.max = this.getValueFromLine(line, MAX_TAG);
+                }
+                if(line.startsWith(ATTEMP_TAG)) {
+                    this.attemps = this.getValueFromLine(line, ATTEMP_TAG);
+                }
+                line = in.readLine();
+            }
         }
         catch (IOException ioExc) {
             ioExc.printStackTrace();
         }
+        System.out.println("min: " + this.min + ", max: " + this.max + ", attemps: " + this.attemps);
     }
 
+    private int getValueFromLine(final String line, final String targetTag) {
+        return Integer.parseInt(line.replace(targetTag + COLON, "").replaceAll(SPACE, ""));
+    }
+    
     public int getAttempts() {
         return this.attemps;
     }
